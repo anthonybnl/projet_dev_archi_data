b98057af41557c9666b913419b1e5956ef44bd2c
Carlot Steven <steven.carlotpro@gmail.com>
2026-04-09 14:27:12 +0200

    Ajout de download_eas.py et download_obligatoire.py

---
+++ pipeline/download_eas.py
@@ -0,0 +1,6 @@
+from download import load_config,parallel_download_routing
+
+feature_name = "AES"
+data_url = load_config()
+feature_data = data_url[feature_name]
+parallel_download_routing(feature_data.values(), feature_name)
